0e04f9d081969c6f5ea9c1951fe877f2b6c00182
Modi97 <102935099+JoseModi97@users.noreply.github.com>
2024-10-02 15:07:45 +0300

    Data Function

--- custom.js
+++ custom.js
@@ -1,17 +1,16 @@
 "use strict";
 
 $(document).ready(function() {
-
-   var array = ["Batman", "Pizza", "Yoda"];
-   var html = '';
-   html += '<ul>'
-   $.each(array, function(index, value){
-    console.log(`${index}: ${value}`);
-    html += '<li>' + value + '</li>';
-   });
-   html += '</ul>'
-   
    $("#EpicButton").click(function(){
-        $("div").html(html);
-   });
+var div = $("div")[0];
+
+   $.data(div, "epic", 
+    {
+        "name": "Frahan",
+        "age": 27
+    }
+   )
+   $("span:first").text($.data(div, "epic"). name)
+   $("span:last").text($.data(div, "epic"). age)
+   })
 });
