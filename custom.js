8614c032c519dc45b6bd5df46c40dd9de4b7ba2b
Modi97 <102935099+JoseModi97@users.noreply.github.com>
2024-10-02 12:45:53 +0300

    GetJson

--- custom.js
+++ custom.js
@@ -2,7 +2,13 @@
 
 $(document).ready(function() {
  $("#EpicButton").click(function(){
-    $("#AJAXDiv").load("textfile.txt")
+    $.getJSON("favourites.json", function(result){
+        console.log(result);
+
+        $.each(result, function(index, value){
+            console.log(`${index} - ${value}`)
+        })
+    })
  })
 
 
