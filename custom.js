5c0ad77305f82f8b6c0f28366f1069a24763315d
Modi97 <102935099+JoseModi97@users.noreply.github.com>
2024-10-02 15:23:17 +0300

    isNumeric

--- custom.js
+++ custom.js
@@ -1,5 +1,9 @@
 "use strict";
 
 $(document).ready(function() {
-console.log($.isWindow(window))
+console.log($.isNumeric(6)) // true
+console.log($.isNumeric("Hello")) // false
+console.log($.isNumeric(-56.835)) // true
+console.log($.isNumeric("67")) // true
+console.log($.isNumeric("67u")) // false
 });
