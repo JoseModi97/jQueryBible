73f571f5bd1836c386fa143440a930606d1199c5
Modi97 <102935099+JoseModi97@users.noreply.github.com>
2024-10-02 15:16:28 +0300

    IsWindow Function

--- custom.js
+++ custom.js
@@ -1,15 +1,5 @@
 "use strict";
 
 $(document).ready(function() {
- var func = function(){
-    console.log(this);
- }
- var object = 
- {
-    food: "Pizza",
- }
-
- var proxyFunction  = $.proxy(func, object);
-
- proxyFunction();
+console.log($.isWindow(window))
 });
