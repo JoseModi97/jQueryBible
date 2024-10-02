e5f5a1f70fad897092f4d26cc3e5446f44cebf08
Modi97 <102935099+JoseModi97@users.noreply.github.com>
2024-10-02 13:52:29 +0300

    Trim String

--- custom.js
+++ custom.js
@@ -1,15 +1,11 @@
 "use strict";
 
 $(document).ready(function() {
- $("#EpicButton").click(function(){
-   $.post("favourites.php",{
-    "food" : "pizza",
-    "game": "Half-Life 3"
-   }   ,function(data, status){
-    console.log(data);
-   })
 
- })
+   var food = " Pizza is the best ";
+
+   console.log(food);
+   console.log(food.trim());
 
 
 });
